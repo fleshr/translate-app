d5df259e92a9319119de47a3a68732c857ab3489
fleshr <sfrxfleshr@gmail.com>
2026-05-26 13:56:46 +0500

    Declare builtin parsers

--- src/shared/constants/parsers.ts
+++ src/shared/constants/parsers.ts
@@ -1,3 +1,23 @@
+import { EntisParser, RenpyTlParser } from "@/shared/lib/parsers";
+import type { ModuleBuiltin } from "../model/module";
 import type { Parser } from "../model/parser";
 
-export const builtinParsers: Record<string, Parser> = {};
+const parsers = [EntisParser, RenpyTlParser];
+
+export const builtinParsersMeta: Record<string, ModuleBuiltin> =
+  Object.fromEntries(
+    parsers.map((parser) => [
+      parser.shortName,
+      {
+        type: "builtin",
+        id: parser.shortName,
+        name: parser.name,
+        version: parser.version,
+        shortName: parser.shortName,
+      },
+    ]),
+  );
+
+export const builtinParsers: Record<string, Parser> = Object.fromEntries(
+  parsers.map((parser) => [parser.shortName, parser]),
+);
