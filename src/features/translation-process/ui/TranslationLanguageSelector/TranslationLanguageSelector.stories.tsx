e6a7ec0ad1e8fa5b68a4a498e6e317b081ef8cab
fleshr <sfrxfleshr@gmail.com>
2026-06-17 18:16:07 +0500

    Disable language selector when translating

--- src/features/translation-process/ui/TranslationLanguageSelector/TranslationLanguageSelector.stories.tsx
+++ src/features/translation-process/ui/TranslationLanguageSelector/TranslationLanguageSelector.stories.tsx
@@ -1,4 +1,7 @@
+import { withStoreState } from "@/shared/lib/storybook";
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import { getTranslationProcessStoreStateMock } from "../../mocks";
+import { useTranslationProcessStore } from "../../model/processStore/store";
 import { TranslationLanguageSelector } from "./TranslationLanguageSelector";
 
 const meta = {
@@ -10,3 +13,12 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const Translating: Story = {
+  decorators: [
+    withStoreState(
+      useTranslationProcessStore,
+      getTranslationProcessStoreStateMock({ status: "translating" }),
+    ),
+  ],
+};
