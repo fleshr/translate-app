b952c24b49b2c73dd3949fe51e8b066236768e7f
fleshr <sfrxfleshr@gmail.com>
2026-06-22 12:01:22 +0500

    Project entity

---
+++ src/entities/project/index.ts
@@ -0,0 +1,6 @@
+export * from "./model/projectStore/actions";
+export * from "./model/projectStore/selectors";
+export { useProjectStore } from "./model/projectStore/store";
+
+export * from "./model/project/schema";
+export * from "./model/project/types";
--- src/entities/project/model/project/types.ts
+++ src/entities/project/model/project/types.ts
@@ -1,4 +1,4 @@
 import type { z } from "zod";
-import type { ProjectSchema } from "./schemas";
+import type { ProjectSchema } from "./schema";
 
 export type Project = z.infer<typeof ProjectSchema>;
